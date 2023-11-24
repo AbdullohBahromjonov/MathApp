a1ad290372507b2d29cd856f003acea02949592b
Abdulloh <abdullohbahromjonv@gmail.com>
2023-11-24 14:41:11 +0500

    Initial Commit

---
+++ Math/MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathApp.swift
+//  Math
+//
+//  Created by Abdulloh on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
